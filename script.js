6d7e92546cbb2b7ec56d32b95a4fc8bf395e498a
sejawal <sejawal77@gmail.com>
2024-03-01 20:55:43 +0500

    javascript variabls and alert

---
+++ script.js
@@ -0,0 +1,85 @@
+            //   ALERT TASKS
+
+// first task  1
+// alert("");
+
+
+//  second 2
+// alert("Error! please enter a valid passsword.")
+
+// third 3
+// alert("Welcome to JS land... \n Happy Coding!") 
+
+// fourth 4
+// alert("Welcome to JS land...")
+
+// fifth 5 
+
+
+//  console.log ("Hello... I can run JS through the brower's console");
+
+// sixth 6 
+
+
+
+
+
+    //   VARIABLES TASKS
+// no 1
+    // var userName;
+
+
+    // no 2
+
+    // var myName = "sejawal ";
+
+// no 3
+//  a
+// var message;
+// b
+// var message = "Hello World";
+// c
+// var message = alert("Hello World");
+
+// no 4
+// var name = alert("John");
+// var age = alert(15);
+// var course = alert("certified Mobile Application Development");
+
+// no 5
+// var fastFood = alert("PIZZA \n PIZZ \n PIZ \n PI \n P ");
+
+// no 6 
+// var email = ("example@gmail.com")
+// alert("my email address is " + email);
+
+// no 7 
+// var book = ("a smarter way to learn javascript");
+
+// alert(book);
+
+
+
+// variables for numbers
+
+// no 1
+
+// var age = 15;
+// alert("I am " + age + "years old");
+
+// no 2 
+
+// var birthYear = 1990;
+// var print = "My birth year is "
+// document.write("<h3>"+print+birthYear+"</h3>");
+// document.write("<h3>declared data type of variable is number </h3>");
+
+// no 3
+
+// var visiterName = "John doe " ;
+
+// var  productTitle = "T-shirts"
+
+// var quantity = 5;
+
+// document.write("<h3>" + visiterName + " " + "ordered" + " " + quantity + " " + productTitle + " " + "on XYZ online store" + "</h3>") 
